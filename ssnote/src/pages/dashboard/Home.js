8f39d0409e3e68e12eb1485b77339a648f93a27a
zhiruo <chenzhiruo20@gmail.com>
2023-07-20 16:33:45 +0800

    tried creating new file

--- ssnote/src/pages/dashboard/Home.js
+++ ssnote/src/pages/dashboard/Home.js
@@ -6,27 +6,33 @@ import file2 from './assets/science.banner2 copy.png';
 import { IoEllipsisVerticalSharp } from 'react-icons/io5';
 import { FaPlus } from 'react-icons/fa'; // Import the FaPlus icon for the plus button
 import PopupMenu from './PopupMenu'; // Import the PopupMenu component
+import NewFilePopup from './NewFilePopup';
 
 function Home() {
+
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  };
+
   const [showMenu1, setShowMenu1] = useState(false);
   const [showMenu2, setShowMenu2] = useState(false);
   const [showPopup, setShowPopup] = useState(false); // State to manage the visibility of the popup
 
-  const toggleMenu1 = (event) => {
-    event.preventDefault(); // Prevent default link behavior
-    event.stopPropagation(); // Prevent link click when clicking the button
-    setShowMenu1(!showMenu1);
-  };
+  // const toggleMenu1 = (event) => {
+  //   event.preventDefault(); // Prevent default link behavior
+  //   event.stopPropagation(); // Prevent link click when clicking the button
+  //   setShowMenu1(!showMenu1);
+  // };
 
-  const toggleMenu2 = (event) => {
-    event.preventDefault(); // Prevent default link behavior
-    event.stopPropagation(); // Prevent link click when clicking the button
-    setShowMenu2(!showMenu2);
-  };
+  // const toggleMenu2 = (event) => {
+  //   event.preventDefault(); // Prevent default link behavior
+  //   event.stopPropagation(); // Prevent link click when clicking the button
+  //   setShowMenu2(!showMenu2);
+  // };
 
-  const togglePopup = () => {
-    setShowPopup(!showPopup); // Toggle the visibility of the popup
-  };
+  // const togglePopup = () => {
+  //   setShowPopup(!showPopup); // Toggle the visibility of the popup
+  // };
 
   return (
     <div className='home'>
@@ -38,15 +44,15 @@ function Home() {
           <Link to='/file1'>
             <img src={file1} alt='file1' />
             <br />
-
+{/* 
             <button onClick={toggleMenu1}>
               <IoEllipsisVerticalSharp />
-            </button>
+            </button> */}
 
             {/* <br /> */}
             <span className='file-title'>File1</span>
           </Link>
-          {showMenu1 && <PopupMenu onClose={() => setShowMenu1(false)} />} {/* Render the pop-up menu conditionally */}
+          {/* {showMenu1 && <PopupMenu onClose={() => setShowMenu1(false)} />} Render the pop-up menu conditionally */}
         </div>
 
         <br />
@@ -55,15 +61,15 @@ function Home() {
           <Link to='/file2'>
             <img src={file2} alt='file2' />
             <br />
-
+{/* 
             <button onClick={toggleMenu2}>
               <IoEllipsisVerticalSharp />
-            </button>
+            </button> */}
 
             {/* <br /> */}
             <span className='file-title'>File2</span>
           </Link>
-          {showMenu2 && <PopupMenu onClose={() => setShowMenu2(false)} />} {/* Render the pop-up menu conditionally */}
+          {/* {showMenu2 && <PopupMenu onClose={() => setShowMenu2(false)} />} Render the pop-up menu conditionally */}
         </div>
       </div>
 
@@ -76,7 +82,7 @@ function Home() {
       {showPopup && (
         <div className='popup-container'>
           {/* Add your content/form for creating a new file here */}
-          <button id='create-folder'><h3>Create New Folder</h3></button>
+          <button id='create-folder'><h3>Create New Note</h3></button>
           <br />
           {/* Add your form or content here */}
           <button onClick={togglePopup}>Close</button>
