3e84b2c2530bdf3c456cb5574498250fb4647a5b
zhiruo <chenzhiruo20@gmail.com>
2023-07-20 13:18:45 +0800

    tried to implement changing password and email

--- ssnote/src/App.js
+++ ssnote/src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/loginpage/LoginPage';
-import Dashboard from './pages/dashboard/Dashboard';
-import Notepage from './pages/notepage/NotePage';
-import Cheatsheets from './pages/dashboard/Cheatsheets';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/loginpage/LoginPage";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Notepage from "./pages/notepage/NotePage";
+import Cheatsheets from "./pages/dashboard/Cheatsheets";
 // import Profile from './pages/dashboard/Profile';
-import Settings from './pages/dashboard/Settings';
-import SignUp from './pages/loginpage/SignUp';
-import ForgotPassword from './pages/loginpage/ForgotPassword';
+import Settings from "./pages/dashboard/Settings";
+import SignUp from "./pages/loginpage/SignUp";
+import ForgotPassword from "./pages/loginpage/ForgotPassword";
 
 const App = () => {
+
   return (
     <Router>
       <Routes>
--- ssnote/src/pages/dashboard/Settings.js
+++ ssnote/src/pages/dashboard/Settings.js
@@ -1,14 +1,79 @@
-import React, { useState } from 'react';
-import Sidebar from './SideBar';
-import './styles/Settings.css';
-import { Link } from 'react-router-dom';
-import LogoutPopup from './LogoutPopup';
-import DeleteAccountPopup from './DeleteAccountPopup';
+import React, { useState } from "react";
+import Sidebar from "./SideBar";
+import "./styles/Settings.css";
+import LogoutPopup from "./LogoutPopup";
+import DeleteAccountPopup from "./DeleteAccountPopup";
+import { getAuth, updatePassword, updateEmail, EmailAuthProvider, reauthenticateWithCredential } from "firebase/auth";
 
 // import LoginPage from './pages/loginpage/LoginPage';
 
-function Settings() {
+const Settings = () => {
   const [isLogoutPopupOpen, setLogoutPopupOpen] = useState(false);
+
+  const [newEmail, setNewEmail] = useState("");
+  const [emailError, setEmailError] = useState(null);
+  const [emailSuccess, setEmailSuccess] = useState(false);
+  const [showEmailInput, setShowEmailInput] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const [newPassword, setNewPassword] = useState("");
+  const [passwordError, setPasswordError] = useState(null);
+  const [passwordSuccess, setPasswordSuccess] = useState(false);
+
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  const handleChangeEmail = () => {
+    setEmailError(null);
+    setEmailSuccess(false);
+
+    if (!newEmail) {
+      setEmailError("Please enter a new email.");
+      return;
+    }
+    if (!password) {
+      setEmailError("Please enter your password to confirm.");
+      return;
+    }
+
+    const credentials = EmailAuthProvider.credential(user.email, password);
+
+    // Call Firebase updateEmail method to change the user's email
+    reauthenticateWithCredential(credentials)
+      .then(() => {
+        // User successfully reauthenticated, now update their email
+        return updateEmail(user, newEmail);
+      })
+      .then(() => {
+        setEmailSuccess(true);
+        setNewEmail("");
+        setPassword("");
+        setShowEmailInput(false); // Close the pop-up after successful update
+      })
+      .catch((error) => {
+        setEmailError(error.message);
+      });
+  };
+
+  const handleChangePassword = () => {
+    setPasswordError(null);
+    setPasswordSuccess(false);
+
+    if (!newPassword) {
+      setPasswordError("Please enter a new password.");
+      return;
+    }
+
+    // Call Firebase updatePassword method to change the user's password
+    updatePassword(user, newPassword)
+      .then(() => {
+        setPasswordSuccess(true);
+        setNewPassword("");
+      })
+      .catch((error) => {
+        setPasswordError(error.message);
+      });
+  };
 
   const handleLogoutPopupToggle = () => {
     setLogoutPopupOpen((prevIsLogoutPopupOpen) => !prevIsLogoutPopupOpen);
@@ -23,7 +88,9 @@ function Settings() {
   const [isDeleteAccountPopupOpen, setDeleteAccountPopupOpen] = useState(false);
 
   const handleDeleteAccountPopupToggle = () => {
-    setDeleteAccountPopupOpen((prevIsDeleteAccountPopupOpen) => !prevIsDeleteAccountPopupOpen);
+    setDeleteAccountPopupOpen(
+      (prevIsDeleteAccountPopupOpen) => !prevIsDeleteAccountPopupOpen
+    );
   };
 
   const handleDeleteAccountConfirm = () => {
@@ -35,46 +102,97 @@ function Settings() {
   return (
     <div>
       <Sidebar />
-      <div className='settings'>
-        <p id='settings-title'>Settings</p>
+      <div className="settings">
+        <p id="settings-title">Settings</p>
         <hr />
 
-        <span id='email'>
-          <span id='email-text'>
+        <span id="email">
+          <span id="email-text">
             Email
             {/* <br /> */}
-            <span id='curr-email'>
-              kristaxyeo@gmail.com
+            <span id="curr-email">
+              {user ? (
+                <span>{user.email}</span>
+              ) : (
+                <span>Please log in to view settings.</span>
+              )}
             </span>
           </span>
-          <button id='email-button'>
-            Change Email
-          </button>
-        </span>
-        <hr className='sub-hr' />
 
-        <span id='password'>
+          {/* Button to toggle the email input pop-up */}
+          {user && (
+            <span>
+              <button id="email-button" onClick={() => setShowEmailInput(true)}>
+                Change Email
+              </button>
+            </span>
+          )}
+
+          {/* Pop-up with the email input */}
+          {showEmailInput && (
+            <div>
+              <input
+                type="password"
+                placeholder="Enter current password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Enter new email"
+                value={newEmail}
+                onChange={(e) => setNewEmail(e.target.value)}
+              />
+              <button onClick={handleChangeEmail}>Update Email</button>
+              <button onClick={() => setShowEmailInput(false)}>Cancel</button>
+              <br />
+              {emailSuccess && <p>Email updated successfully.</p>}
+              {emailError && <p>Error: {emailError}</p>}
+            </div>
+          )}
+        </span>
+        <hr className="sub-hr" />
+
+        <span id="password">
           Password
-          <button id='password-button'>
+          <button id="password-button" onClick={handleChangePassword}>
             Change Password
           </button>
+          <br />
+          {passwordSuccess && <p>Password updated successfully.</p>}
+          {passwordError && <p>Error: {passwordError}</p>}
+          <input
+            type="password"
+            placeholder="Enter new password"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+          />
         </span>
-        <hr className='sub-hr' />
+        <hr className="sub-hr" />
 
-        <button id='logout' onClick={handleLogoutPopupToggle}>
+        <button id="logout" onClick={handleLogoutPopupToggle}>
           Log Out
         </button>
-        
-        {isLogoutPopupOpen && <LogoutPopup onCancel={handleLogoutPopupToggle} onConfirm={handleLogoutConfirm} />}
-        
-        <button id='delete-account' onClick={handleDeleteAccountPopupToggle}>
+
+        {isLogoutPopupOpen && (
+          <LogoutPopup
+            onCancel={handleLogoutPopupToggle}
+            onConfirm={handleLogoutConfirm}
+          />
+        )}
+
+        <button id="delete-account" onClick={handleDeleteAccountPopupToggle}>
           Delete Account
         </button>
-        {isDeleteAccountPopupOpen && <DeleteAccountPopup onCancel={handleDeleteAccountPopupToggle} onDelete={handleDeleteAccountConfirm} />}
-
+        {isDeleteAccountPopupOpen && (
+          <DeleteAccountPopup
+            onCancel={handleDeleteAccountPopupToggle}
+            onDelete={handleDeleteAccountConfirm}
+          />
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Settings;
