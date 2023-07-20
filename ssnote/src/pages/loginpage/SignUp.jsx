da332ae79dd1fa42d2437d4dcfe636f78240c14d
zhiruo <chenzhiruo20@gmail.com>
2023-07-20 15:25:49 +0800

    after signing up link to login page

--- ssnote/src/pages/loginpage/SignUp.jsx
+++ ssnote/src/pages/loginpage/SignUp.jsx
@@ -1,20 +1,31 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../../firebase";
+import { Modal, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showModal, setShowModal] = useState(false);
+
+    const navigate = useNavigate();
 
   const signUp = (e) => {
     e.preventDefault();
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         console.log(userCredential);
+        setShowModal(true);
       })
       .catch((error) => {
         console.log(error);
       });
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    navigate("/"); // Redirect to login page after closing the modal
   };
 
   return (
@@ -35,6 +46,21 @@ const SignUp = () => {
         ></input>
         <button type="submit">Sign Up</button>
       </form>
+
+      {/* Modal for success message */}
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Sign Up Successful</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          You have successfully signed up! Click the button below to proceed to the login page.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
