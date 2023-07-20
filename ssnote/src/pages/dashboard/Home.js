b1906ca3d633e0f2ac4bb07f88cd8cac9f4e9029
zhiruo <chenzhiruo20@gmail.com>
2023-07-20 16:50:38 +0800

    cleaned up code

--- ssnote/src/pages/dashboard/Home.js
+++ ssnote/src/pages/dashboard/Home.js
@@ -3,10 +3,7 @@ import { Link } from 'react-router-dom';
 import './styles/Home.css';
 import file1 from './assets/math-curriculum copy.webp';
 import file2 from './assets/science.banner2 copy.png';
-import { IoEllipsisVerticalSharp } from 'react-icons/io5';
 import { FaPlus } from 'react-icons/fa'; // Import the FaPlus icon for the plus button
-import PopupMenu from './PopupMenu'; // Import the PopupMenu component
-import NewFilePopup from './NewFilePopup';
 
 function Home() {
 
@@ -14,25 +11,7 @@ function Home() {
     setShowPopup(!showPopup);
   };
 
-  const [showMenu1, setShowMenu1] = useState(false);
-  const [showMenu2, setShowMenu2] = useState(false);
   const [showPopup, setShowPopup] = useState(false); // State to manage the visibility of the popup
-
-  // const toggleMenu1 = (event) => {
-  //   event.preventDefault(); // Prevent default link behavior
-  //   event.stopPropagation(); // Prevent link click when clicking the button
-  //   setShowMenu1(!showMenu1);
-  // };
-
-  // const toggleMenu2 = (event) => {
-  //   event.preventDefault(); // Prevent default link behavior
-  //   event.stopPropagation(); // Prevent link click when clicking the button
-  //   setShowMenu2(!showMenu2);
-  // };
-
-  // const togglePopup = () => {
-  //   setShowPopup(!showPopup); // Toggle the visibility of the popup
-  // };
 
   return (
     <div className='home'>
@@ -44,15 +23,9 @@ function Home() {
           <Link to='/file1'>
             <img src={file1} alt='file1' />
             <br />
-{/* 
-            <button onClick={toggleMenu1}>
-              <IoEllipsisVerticalSharp />
-            </button> */}
 
-            {/* <br /> */}
             <span className='file-title'>File1</span>
           </Link>
-          {/* {showMenu1 && <PopupMenu onClose={() => setShowMenu1(false)} />} Render the pop-up menu conditionally */}
         </div>
 
         <br />
@@ -61,15 +34,8 @@ function Home() {
           <Link to='/file2'>
             <img src={file2} alt='file2' />
             <br />
-{/* 
-            <button onClick={toggleMenu2}>
-              <IoEllipsisVerticalSharp />
-            </button> */}
-
-            {/* <br /> */}
             <span className='file-title'>File2</span>
           </Link>
-          {/* {showMenu2 && <PopupMenu onClose={() => setShowMenu2(false)} />} Render the pop-up menu conditionally */}
         </div>
       </div>
 
