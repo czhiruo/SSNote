61c5a877ecd18c8b89b0737d3e4c98b11935c2dc
zhiruo <chenzhiruo20@gmail.com>
2023-07-30 00:15:02 +0800

    aligned the filtered text

--- ssnote/src/pages/notepage/DisplayContent.js
+++ ssnote/src/pages/notepage/DisplayContent.js
@@ -1,13 +1,19 @@
 import React from "react";
+import "./DisplayContent.css";
 
 const DisplayContent = ({ filteredData }) => {
-    console.log(filteredData);
+  console.log(filteredData);
   return (
     <div>
-      <h2>Summary</h2>
+      <h2 className="title">Summary</h2>
+      {/* Apply the "indented-paragraph" class to the <p> elements */}
       {filteredData.blocks.map((block, index) => (
-        <p key={index}>{block.text}</p>
+        <p key={index} className="indented-paragraph">
+          {block.text}
+        </p>
       ))}
+      <br></br>
+      <br></br>
     </div>
   );
 };
